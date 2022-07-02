8c2ed5d7f69576bdfad28b0145d01a2438d2ea31
Rina Takikawa <takikawa.rina@gmail.com>
2022-07-02 11:09:16 -0400

    initial commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,10 +4,7 @@ export default function Document() {
     return (
         <Html>
             <Head>
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Eczar:wght@400;500;600;700;800&family=Inter:wght@300;400;500;600;700&display=swap"
-                    rel="stylesheet"
-                />
+            <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&display=swap" rel="stylesheet"/>
             </Head>
             <body>
                 <Main />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,27 @@
+import Head from "next/head";
+
 export default function Index() {
     return (
         <>
-            <h1>Hello world!</h1>
-            <p>This is a starter template you can use for Web Dev for Makers projects.</p>
-            <img src="/logo.png" alt="Web Dev for Makers Logo" />
+        <Head>
+            <title>Rina Takikawa</title>
+            <meta name="description" content= "Welcome to the club"/>
+        </Head>
+        <div id="container">
+            <div id="top-section">
+                 <img src="/desktoplogo.png" id="logo-pic"/>
+                 <h1> Hi, I'm Rina! <br/> Welcome to the club.</h1>
+                 <p>Currently <a href="https://www.amoochlife.com/"> at Mooch </a> A budgeting app powered by Blockchain  </p>
+            </div>
+            
+        </div>
+           
+    
+            <img src="/teampic.jpg" id="team-pic"/>
+            <img src="/profile.jpg" id="profile-pic"/>
+            <img src="/coworking.jpg" id="cowork-pic"/>
+            <img src="/florida.jpg" id="florida-pic"/>
+            <img src="/ballet.jpg" id="ballet-pic"/>
         </>
     );
 }
